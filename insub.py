37d54fa03b8c13b0e4d4792dce5ac01a8dcb1388
Chris Jones <cjones@gmail.com>
2008-12-13 08:38:28 +0000

    stubbed

--- insub.py
+++ insub.py
@@ -59,11 +59,137 @@ class Insub(object):
             self.__class__.filters.append((func, self.options))
             return func
 
+    @filter()
+    def ver(self, lines):
+        return lines
+
+    @filter()
+    def stdin(self, lines):
+        return lines
+
+    @filter()
+    def execute(self, lines):
+        return lines
+
+    @filter()
+    def slurp(self, lines):
+        return lines
+
+    @filter()
+    def spook(self, lines):
+        return lines
+
+    @filter()
+    def jive(self, lines):
+        return lines
+
+    @filter()
+    def scramble(self, lines):
+        return lines
+
+    @filter()
+    def leet(self, lines):
+        return lines
+
+    @filter()
+    def reverse(self, lines):
+        return lines
+
+    @filter()
+    def jigs(self, lines):
+        return lines
+
+    @filter()
+    def sine(self, lines):
+        return lines
+
+    @filter()
+    def diagonal(self, lines):
+        return lines
+
+    @filter()
+    def popeye(self, lines):
+        return lines
+
+    @filter()
+    def matrix(self, lines):
+        return lines
+
+    @filter()
+    def figlet(self, lines):
+        return lines
+
+    @filter()
+    def banner(self, lines):
+        return lines
+
+    @filter()
+    def hug(self, lines):
+        return lines
+
+    @filter()
+    def rotate(self, lines):
+        return lines
+
+    @filter()
+    def wrap(self, lines):
+        return lines
+
+    @filter()
+    def chalkboard(self, lines):
+        return lines
+
+    @filter()
+    def checker(self, lines):
+        return lines
+
+    @filter()
+    def cow(self, lines):
+        return lines
+
+    @filter()
+    def flip(self, lines):
+        return lines
+
+    @filter()
+    def outline(self, lines):
+        return lines
+
+    @filter()
+    def rainbow(self, lines):
+        return lines
+
+    @filter()
+    def tree(self, lines):
+        return lines
+
+    @filter()
+    def blink(self, lines):
+        return lines
+
+    @filter()
+    def ircii_fake(self, lines):
+        return lines
+
+    @filter()
+    def ircii_drop(self, lines):
+        return lines
+
+    @filter()
+    def prefix(self, lines):
+        return lines
+
+    @filter()
+    def strip(self, lines):
+        return lines
+
 
 def main():
     # dest metavar default action type nargs const choices callback help
     # store[_(const|true|false)] append[_const] count callback
     # string int long float complex choice
+
+    # add filter options to arg parser
     parser = OptionParser(version=__version__)
     filters = []
     add_filter = lambda option, key, val, parser, func: filters.append(func)
@@ -81,7 +207,9 @@ def main():
         if func in filters and func not in opts.filters:
             opts.filters.append(func)
 
+    # process data
     insub = Insub(**opts.__dict__)
+    print insub.process()
 
     return 0
 
