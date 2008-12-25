6e2498431f8500f2147e5a934b4e41c99b03543d
Chris Jones <cjones@gmail.com>
2008-12-25 17:48:01 +0000

    experimental flesh tone filter

--- color/make_image_map.py
+++ color/make_image_map.py
@@ -1,50 +1,72 @@
 #!/usr/bin/env python
 
+"""Generate a colormap from an image"""
+
 from __future__ import with_statement
 from optparse import OptionParser
 import sys
 import PIL.Image
 import os
 import math
-
-"""Generate a colormap from an image"""
+import time
 
 # approximate RGB values of our available ANSI colors, based on iTerm
-iterm = {'d': (0, 0, 0),
-         'D': (85, 85, 85),
-         'r': (205, 0, 0),
-         'R': (255, 80, 86),
-         'g': (0, 192, 0),
-         'G': (0, 255, 69),
-         'y': (184, 190, 0),
-         'Y': (252, 255, 71),
-         'b': (24, 0, 192),
-         'B': (92, 75, 255),
-         'm': (207, 0, 192),
-         'M': (255, 69, 255),
-         'c': (0, 189, 188),
-         'C': (0, 255, 255),
-         'w': (187, 187, 187),
-         'W': (255, 255, 255)}
+iterm = (('d', (0, 0, 0)),
+         ('D', (85, 85, 85)),
+         ('r', (205, 0, 0)),
+         ('R', (255, 80, 86)),
+         ('g', (0, 192, 0)),
+         ('G', (0, 255, 69)),
+         ('y', (184, 190, 0)),
+         ('Y', (252, 255, 71)),
+         ('b', (24, 0, 192)),
+         ('B', (92, 75, 255)),
+         ('m', (207, 0, 192)),
+         ('M', (255, 69, 255)),
+         ('c', (0, 189, 188)),
+         ('C', (0, 255, 255)),
+         ('w', (187, 187, 187)),
+         ('W', (255, 255, 255)))
 
-# more "pure" version of RGB primary colors, i guess
-pure = {'d': (0, 0, 0),
-        'D': (85, 85, 85),
-        'r': (128, 0, 0),
-        'R': (255, 0, 0),
-        'g': (0, 128, 0),
-        'G': (0, 255, 0),
-        'y': (128, 128, 0),
-        'Y': (252, 255, 0),
-        'b': (0, 0, 128),
-        'B': (0, 0, 255),
-        'm': (128, 0, 128),
-        'M': (255, 0, 255),
-        'c': (0, 128, 128),
-        'C': (0, 255, 255),
-        'w': (171, 171, 171),
-        'W': (255, 255, 255)}
+# more "pure" version of RGB primary colors (high contrast between light/dark)
+pure = (('d', (0, 0, 0)),
+        ('D', (85, 85, 85)),
+        ('r', (128, 0, 0)),
+        ('R', (255, 0, 0)),
+        ('g', (0, 128, 0)),
+        ('G', (0, 255, 0)),
+        ('y', (128, 128, 0)),
+        ('Y', (255, 255, 0)),
+        ('b', (0, 0, 128)),
+        ('B', (0, 0, 255)),
+        ('m', (128, 0, 128)),
+        ('M', (255, 0, 255)),
+        ('c', (0, 128, 128)),
+        ('C', (0, 255, 255)),
+        ('w', (171, 171, 171)),
+        ('W', (255, 255, 255)))
 
+
+def fix_flesh_tone(rgb):
+    """Transforms fleshtones into something more yellow (experimental)"""
+    # XXX the range it considers flesh is pretty wide and can cause some
+    # unwanted blending.  a better solution might be to tighten the range
+    # it considers flesh and allow the user to specify a skew that relates
+    # to the skin tone of the subject.  this might even be auto-detected by
+    # a color count.  right now this is better than nothing though.  without
+    # this adjustment, most photographs of people turn into a slop of grey
+    # another candidate for adjustment is the +37 to r/g.. the point of this
+    # is to put the midpoint of flesh tones around the midpoint of ansi
+    # dark yellow/bright yellow.  then lighting or tanlines will show up
+    # highlighted. however this will only work with a narrow skin color range
+    rgb = list(rgb)
+    if (rgb[0] >= 147 and rgb[0] <= 255 and
+        rgb[1] >= 107 and rgb[1] <= 217 and
+        rgb[2] >= 99  and rgb[2] <= 198):
+        rgb[0] += 37
+        rgb[1] += 37
+        rgb[2] = 0
+    return rgb
 
 
 def euclidean_color_dist(x, y):
@@ -64,39 +86,51 @@ def weighted_color_dist(x, y):
 
 
 def main():
-    parser = OptionParser()
-    parser.add_option('-w', '--weighted', dest='distance',
-                      default=euclidean_color_dist, action='store_const',
-                      const=weighted_color_dist, help='alt color aprox')
-    parser.add_option('-c', '--correct', default=False, action='store_true',
-                      help='correct aspect ratio')
-    parser.add_option('-i', '--iterm', dest='palette', default=pure,
+    parser = OptionParser(usage='%prog [-lci] [-o <map>] <image>')
+    parser.add_option('-o', dest='output', metavar='<file>', help='output map')
+    parser.add_option('-c', dest='correct', default=False, action='store_true',
+                      help='correct aspect ratio (prevent stretched map)')
+    parser.add_option('-f', dest='flesh', default=False, action='store_true',
+                      help='make flesh tones more yellow')
+    parser.add_option('-i', dest='palette', default=pure,
                       action='store_const', const=iterm,
-                      help='use iterm palette')
+                      help='use iterm palette (darker)')
+    parser.add_option('-l', dest='distance',
+                      default=weighted_color_dist, action='store_const',
+                      const=euclidean_color_dist, help='linear color distance')
     opts, args = parser.parse_args()
 
-    for src in args:
-        name = os.path.basename(src)
-        image = PIL.Image.open(src)
-        width, height = image.size
-        if opts.correct:
-            height = int(height * .4286)
-            image = image.resize((width, height))
-        if image.mode == 'P':
-            palette = image.getpalette()
-        output = []
+    # check args
+    if len(args) != 1:
+        parser.print_help()
+        return 1
+    src = args[0]
+    if not opts.output:
+        opts.output = os.path.splitext(os.path.basename(src))[0] + '.map'
+
+    # create map
+    start = time.time()
+    image = PIL.Image.open(src)
+    width, height = image.size
+    if opts.correct:
+        height = int(height * .4286)
+        image = image.resize((width, height))
+    if image.mode == 'P':
+        palette = image.getpalette()
+        palette = [palette[i:i + 3] for i in xrange(0, len(palette), 3)]
+    with open(opts.output, 'wb') as fp:
         for y in xrange(height):
             for x in xrange(width):
                 rgb = image.getpixel((x, y))
                 if image.mode == 'P':
-                    i = rgb * 3
-                    rgb = palette[i:i + 3]
-                output.append(sorted(
+                    rgb = palette[rgb]
+                if opts.flesh:
+                    rgb = fix_flesh_tone(rgb)
+                fp.write(sorted(
                     [(opts.distance(rgb, code_rgb), code)
-                     for code, code_rgb in opts.palette.iteritems()])[0][1])
-            output.append('\n')
-        with open(os.path.splitext(name)[0] + '.map', 'wb') as fp:
-            fp.write(''.join(output))
+                     for code, code_rgb in opts.palette])[0][1])
+            fp.write('\n')
+    print 'elapsed: %.2f' % (time.time() - start)
 
     return 0
 
@@ -104,6 +138,7 @@ def main():
 if __name__ == '__main__':
     try:
         import psyco
+        psyco.cannotcompile(PIL.Image.open)
         psyco.full()
     except ImportError:
         pass
